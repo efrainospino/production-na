14702b24a4b932fe4df01f1e772f2878611ce2f3
Efrain Ospino <efrainospino14@gmail.com>
2023-02-04 17:30:19 -0500

    nuevo commit

--- index.js
+++ index.js
@@ -24,14 +24,31 @@ app.use(bodyParser.urlencoded({extended: true}));
 //habilitar uploads
 app.use('/uploads', express.static('uploads'));
 
+//definir dominios
+const whiteList = [process.env.FRONTEND_URL]
+const corsOptions ={
+    origin: (origin, callback) => {
+        //revisar si la peticion viene de un servidor que esta en la whiteList
+        const existe = whiteList.some(dominio => dominio === origin);
+        if(existe){
+            callback(null, true);
+        }else{
+            callback(new Error ('No permitido por CORS'));
+        }
+    }
+}
+
 //habilitar cors
-app.use(cors());
+app.use(cors(corsOptions));
 
 //rutas de la app 
 app.use('/', routes());
 
-
-//puerto
+//puerto y host
+const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 5001
-app.listen(port);
+//iniciar app
+app.listen(port, host, () =>{
+    console.log('el servidor esta funcionando');
+});
 
